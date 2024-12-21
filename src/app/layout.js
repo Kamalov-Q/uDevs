23d0aa9350fe53c6e829e79aa0e3f3ba27eaa02b
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2024-12-21 12:36:12 +0500

    Changed again

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,10 @@
-/* eslint-disable @next/next/no-sync-scripts */
-import { Inter } from "next/font/google";
 import "./globals.css";
 /* @kamalovquvomiddin  Kamalov Quvomiddin Fully ready 21.07.2024 23:17 */
-
+/* @kamalovq Kamalov Quvomiddin Fully updated 21.12.2024 12:32 */
 export const Metadata = {
   title: "Kamalov Quvomiddin Udevs",
   description: "Kamalov Quvomiddin Udevsni qilgan edim",
+
   keywords:
     "Kamalov, Quvomiddin, Udevs, Kamalov Quvomiddin Udevs, Kamalov Quvomiddin, Kamalov Udevs, Quvomiddin Udevs",
 };
@@ -15,6 +14,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+        <noscript>
+          Kamalov Quvomiddin Sevarani Sevardi bir vaqtlar hozir esa yuq. Men
+          qaytishga tayyorman
+        </noscript>
       </head>
       <body>{children}</body>
     </html>
