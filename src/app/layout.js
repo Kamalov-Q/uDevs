542ad4804bef702578fe88dc76746b834b55b7cd
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2024-12-21 12:31:42 +0500

    Changed again

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 /* @kamalovquvomiddin  Kamalov Quvomiddin Fully ready 21.07.2024 23:17 */
+
+export const Metadata = {
+  title: "Kamalov Quvomiddin Udevs",
+  description: "Kamalov Quvomiddin Udevsni qilgan edim",
+  keywords:
+    "Kamalov, Quvomiddin, Udevs, Kamalov Quvomiddin Udevs, Kamalov Quvomiddin, Kamalov Udevs, Quvomiddin Udevs",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
